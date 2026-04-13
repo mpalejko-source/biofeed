3ac949bdd57cfb3294c653b85e2f86a59377f1ef
mpalejko-source <acegrouppl@gmail.com>
2026-04-13 11:52:17 +0200

    Create reports-data.js

---
+++ podsumowanie-sbm/reports-data.js
@@ -0,0 +1,12 @@
+window.REPORTS = [
+  {
+    file: "marzec2026.pdf",
+    title: "Marzec 2026",
+    published: "2026-04-13"
+  },
+  {
+    file: "luty2026.pdf",
+    title: "Luty 2026",
+    published: "2026-03-12"
+  }
+];
